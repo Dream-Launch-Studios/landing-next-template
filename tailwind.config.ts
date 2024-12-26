da7f79e3e85e3b38526fa33561136537210d7e2b
ashishmohanty10 <ashishmohaty10@gmail.com>
2024-12-26 18:12:18 +0530

    feat: add companies marquee

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -68,29 +68,13 @@ export default {
     },
 
     animation: {
-      "shimmer-slide":
-        "shimmer-slide var(--speed) ease-in-out infinite alternate",
-      "spin-around": "spin-around calc(var(--speed) * 2) infinite linear",
+      "infinite-scroll":
+        "infinite-scroll var(--animation-delay) linear infinite forwards",
     },
     keyframes: {
-      "spin-around": {
-        "0%": {
-          transform: "translateZ(0) rotate(0)",
-        },
-        "15%, 35%": {
-          transform: "translateZ(0) rotate(90deg)",
-        },
-        "65%, 85%": {
-          transform: "translateZ(0) rotate(270deg)",
-        },
-        "100%": {
-          transform: "translateZ(0) rotate(360deg)",
-        },
-      },
-      "shimmer-slide": {
-        to: {
-          transform: "translate(calc(100cqw - 100%), 0)",
-        },
+      "infinite-scroll": {
+        from: { transform: "translateX(0)" },
+        to: { transform: "translateX(-50%)" },
       },
     },
   },
