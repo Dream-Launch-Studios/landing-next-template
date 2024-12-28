77147c3d265ae24fa44a2711f414dc699b130a9c
ashishmohanty10 <ashishmohaty10@gmail.com>
2024-12-29 01:53:06 +0530

    refactor: unused code

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -61,71 +61,44 @@ export const reviews = [
   },
 ];
 
-export const testimonials = [
-  [
-    {
-      content:
-        "TaxPal is so easy to use I can’t help but wonder if it’s really doing the things the government expects me to do.",
-      author: {
-        name: "Sheryl Berge",
-        role: "CEO at Lynch LLC",
-        image:
-          "https://images.unsplash.com/photo-1579017331263-ef82f0bbc748?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=685&q=80",
-      },
-    },
-    {
-      content:
-        "I’m trying to get a hold of someone in support, I’m in a lot of trouble right now and they are saying it has something to do with my books. Please get back to me right away.",
-      author: {
-        name: "Amy Hahn",
-        role: "Director at Velocity Industries",
-        image:
-          "https://images.unsplash.com/photo-1623605931891-d5b95ee98459?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=640&q=80",
-      },
-    },
-  ],
-  [
-    {
-      content:
-        "The best part about TaxPal is every time I pay my employees, my bank balance doesn’t go down like it used to. Looking forward to spending this extra cash when I figure out why my card is being declined.",
-      author: {
-        name: "Leland Kiehn",
-        role: "Founder of Kiehn and Sons",
-        image:
-          "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-      },
-    },
-    {
-      content:
-        "There are so many things I had to do with my old software that I just don’t do at all with TaxPal. Suspicious but I can’t say I don’t love it.",
-      author: {
-        name: "Erin Powlowski",
-        role: "COO at Armstrong Inc",
-        image:
-          "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-      },
-    },
-  ],
-  [
-    {
-      content:
-        "I used to have to remit tax to the EU and with TaxPal I somehow don’t have to do that anymore. Nervous to travel there now though.",
-      author: {
-        name: "Peter Renolds",
-        role: "Founder of West Inc",
-        image:
-          "https://images.unsplash.com/photo-1540569014015-19a7be504e3a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
-      },
-    },
-    {
-      content:
-        "This is the fourth email I’ve sent to your support team. I am literally being held in jail for tax fraud. Please answer your damn emails, this is important.",
-      author: {
-        name: "Amy Hahn",
-        role: "Director at Velocity Industries",
-        image:
-          "https://images.unsplash.com/photo-1540569014015-19a7be504e3a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
-      },
-    },
-  ],
+export const footerLinks = [
+  {
+    title: "Products",
+    links: [
+      { title: "Features", href: "#" },
+      { title: "Integrations", href: "#" },
+      { title: "Pricing", href: "#" },
+      { title: "Changelog", href: "#" },
+      { title: "Docs", href: "#" },
+      { title: "Download", href: "#" },
+    ],
+  },
+  {
+    title: "Company",
+    links: [
+      { title: "About us", href: "#" },
+      { title: "Blog", href: "#" },
+      { title: "Career", href: "#" },
+      { title: "Customers", href: "#" },
+      { title: "Brand", href: "#" },
+    ],
+  },
+
+  {
+    title: "Resources",
+    links: [
+      { title: "Community", href: "#" },
+      { title: "Contact", href: "#" },
+      { title: "DPA", href: "#" },
+      { title: "Terms of service", href: "#" },
+    ],
+  },
+  {
+    title: "Developers",
+    links: [
+      { title: "API", href: "#" },
+      { title: "Status", href: "#" },
+      { title: "Github", href: "#" },
+    ],
+  },
 ];
